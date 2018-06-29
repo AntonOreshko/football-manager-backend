e02fb121234e7bf7e62164897cc35c9c182123b8
araxis <oreshko2010@gmail.com>
2018-06-29 09:36:47 +0300

    engine settings

--- Engine/Program.cs
+++ Engine/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RepositoryLayer.EntityFramework.Context;
+using Microsoft.Extensions.DependencyInjection;
+using Hangfire;
+using Hangfire.Storage;
 
 namespace Engine
 {
@@ -6,7 +13,43 @@ namespace Engine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connectionName = "HomeConnection";
+            //var connectionName = "WorkConnection";
+
+            // config
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage(config.GetConnectionString(connectionName));
+
+            // services
+            var services = new ServiceCollection();
+
+            services.AddLogging();
+            services.AddDbContext<FootballManagerContext>(o => o.UseSqlServer(config.GetConnectionString(connectionName)));
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.WriteLine("Hangfire Server started. Press any key to exit...");
+
+                RecurringJob.AddOrUpdate(() => Console.Write("Easy!"), Cron.Minutely);
+
+                Console.ReadLine();
+            }
+
+            using (var connection = JobStorage.Current.GetConnection())
+            {
+                foreach (var recurringJob in connection.GetRecurringJobs())
+                {
+                    RecurringJob.RemoveIfExists(recurringJob.Id);
+                }
+            }
         }
     }
 }
