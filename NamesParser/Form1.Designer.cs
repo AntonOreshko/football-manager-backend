6582e804bdb67f3c0daf6915731421b4905b7fc8
araxis <oreshko2010@gmail.com>
2018-06-29 14:17:51 +0300

    names parser

---
+++ NamesParser/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace NamesParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadHtmlButton = new System.Windows.Forms.Button();
+            this.url = new System.Windows.Forms.TextBox();
+            this.FirstNameRadioButton = new System.Windows.Forms.RadioButton();
+            this.LastNameRadioButton = new System.Windows.Forms.RadioButton();
+            this.CountryComboBox = new System.Windows.Forms.ComboBox();
+            this.JoinFilesButton = new System.Windows.Forms.Button();
+            this.JoinFilesAndClearNamesForSelectedCountryButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoadHtmlButton
+            // 
+            this.LoadHtmlButton.Location = new System.Drawing.Point(12, 84);
+            this.LoadHtmlButton.Name = "LoadHtmlButton";
+            this.LoadHtmlButton.Size = new System.Drawing.Size(258, 21);
+            this.LoadHtmlButton.TabIndex = 0;
+            this.LoadHtmlButton.Text = "Save Names";
+            this.LoadHtmlButton.UseVisualStyleBackColor = true;
+            this.LoadHtmlButton.Click += new System.EventHandler(this.LoadHtmlButton_Click);
+            // 
+            // url
+            // 
+            this.url.Location = new System.Drawing.Point(12, 8);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(643, 20);
+            this.url.TabIndex = 1;
+            // 
+            // FirstNameRadioButton
+            // 
+            this.FirstNameRadioButton.AutoSize = true;
+            this.FirstNameRadioButton.Location = new System.Drawing.Point(12, 61);
+            this.FirstNameRadioButton.Name = "FirstNameRadioButton";
+            this.FirstNameRadioButton.Size = new System.Drawing.Size(75, 17);
+            this.FirstNameRadioButton.TabIndex = 2;
+            this.FirstNameRadioButton.TabStop = true;
+            this.FirstNameRadioButton.Text = "First Name";
+            this.FirstNameRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // LastNameRadioButton
+            // 
+            this.LastNameRadioButton.AutoSize = true;
+            this.LastNameRadioButton.Location = new System.Drawing.Point(93, 61);
+            this.LastNameRadioButton.Name = "LastNameRadioButton";
+            this.LastNameRadioButton.Size = new System.Drawing.Size(76, 17);
+            this.LastNameRadioButton.TabIndex = 3;
+            this.LastNameRadioButton.TabStop = true;
+            this.LastNameRadioButton.Text = "Last Name";
+            this.LastNameRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // CountryComboBox
+            // 
+            this.CountryComboBox.FormattingEnabled = true;
+            this.CountryComboBox.Location = new System.Drawing.Point(13, 34);
+            this.CountryComboBox.Name = "CountryComboBox";
+            this.CountryComboBox.Size = new System.Drawing.Size(257, 21);
+            this.CountryComboBox.TabIndex = 4;
+            // 
+            // JoinFilesButton
+            // 
+            this.JoinFilesButton.Location = new System.Drawing.Point(13, 140);
+            this.JoinFilesButton.Name = "JoinFilesButton";
+            this.JoinFilesButton.Size = new System.Drawing.Size(257, 23);
+            this.JoinFilesButton.TabIndex = 5;
+            this.JoinFilesButton.Text = "Join files and clear names";
+            this.JoinFilesButton.UseVisualStyleBackColor = true;
+            this.JoinFilesButton.Click += new System.EventHandler(this.JoinFilesButton_Click);
+            // 
+            // JoinFilesAndClearNamesForSelectedCountryButton
+            // 
+            this.JoinFilesAndClearNamesForSelectedCountryButton.Location = new System.Drawing.Point(13, 111);
+            this.JoinFilesAndClearNamesForSelectedCountryButton.Name = "JoinFilesAndClearNamesForSelectedCountryButton";
+            this.JoinFilesAndClearNamesForSelectedCountryButton.Size = new System.Drawing.Size(257, 23);
+            this.JoinFilesAndClearNamesForSelectedCountryButton.TabIndex = 6;
+            this.JoinFilesAndClearNamesForSelectedCountryButton.Text = "Join files and clear names for selected country";
+            this.JoinFilesAndClearNamesForSelectedCountryButton.UseVisualStyleBackColor = true;
+            this.JoinFilesAndClearNamesForSelectedCountryButton.Click += new System.EventHandler(this.JoinFilesAndClearNamesForSelectedCountryButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 184);
+            this.Controls.Add(this.JoinFilesAndClearNamesForSelectedCountryButton);
+            this.Controls.Add(this.JoinFilesButton);
+            this.Controls.Add(this.CountryComboBox);
+            this.Controls.Add(this.LastNameRadioButton);
+            this.Controls.Add(this.FirstNameRadioButton);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.LoadHtmlButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button LoadHtmlButton;
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.RadioButton FirstNameRadioButton;
+        private System.Windows.Forms.RadioButton LastNameRadioButton;
+        private System.Windows.Forms.ComboBox CountryComboBox;
+        private System.Windows.Forms.Button JoinFilesButton;
+        private System.Windows.Forms.Button JoinFilesAndClearNamesForSelectedCountryButton;
+    }
+}
+
---
+++ NamesParser/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+
+namespace NamesParser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CountryComboBox.DataSource = Enum.GetValues(typeof(Country));
+        }
+
+        private void LoadHtmlButton_Click(object sender, System.EventArgs e)
+        {
+            string namePattern = "(name)";
+            string givenNamePattern = "(given name)";
+            string surnamePattern = "(surname)";
+            string disambiguationPattern = "(disambiguation)";
+
+            var htmlWeb = new HtmlWeb();
+            var document = htmlWeb.Load(url.Text);
+
+            var names = new List<string>();
+
+            var listNode = document.GetElementbyId("mw-pages");
+            foreach (var liNode in listNode.SelectNodes(".//li"))
+            {
+                var name = liNode.InnerText;
+
+                if (
+                       liNode.InnerText.Contains(namePattern) 
+                    || liNode.InnerText.Contains(givenNamePattern)               
+                    || liNode.InnerText.Contains(surnamePattern)                                    
+                    || liNode.InnerText.Contains(disambiguationPattern)
+                   )
+                {
+                    name = liNode.InnerText
+                        .Replace(namePattern, string.Empty)
+                        .Replace(givenNamePattern, string.Empty)
+                        .Replace(surnamePattern, string.Empty)
+                        .Replace(disambiguationPattern, string.Empty);
+
+                }
+
+                name = name.Trim();
+                names.Add(name);
+            }
+
+            var filename = FirstNameRadioButton.Checked ? "FirstNames" : "LastNames";
+
+            if (File.Exists(@"D:/Names/" + CountryComboBox.SelectedValue + "." + filename + ".json"))
+            {
+                int counter = 2;
+
+                while (File.Exists(@"D:/Names/" + CountryComboBox.SelectedValue + "." + filename + "." + counter + ".json"))
+                {
+                    counter++;
+                }
+
+                filename += "." + counter;
+            }
+
+            filename += ".json";
+
+            using (var file = File.CreateText(@"D:/Names/" + CountryComboBox.SelectedValue + "." + filename))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, names);
+            }
+        }
+
+        private void JoinFilesButton_Click(object sender, EventArgs e)
+        {
+            foreach (var value in Enum.GetValues(typeof(Country)))
+            {
+                JoinFilesForCountry(value.ToString());
+            }
+        }
+
+        private void JoinFilesAndClearNamesForSelectedCountryButton_Click(object sender, EventArgs e)
+        {
+            var value = CountryComboBox.SelectedValue.ToString();
+
+            JoinFilesForCountry(value);
+        }
+
+        private void JoinFilesForCountry(string value)
+        {
+            var shitCount = 0;
+            string pattern = @"(\(.+?\))";
+
+            //------------
+            var filesPathes1 = Directory.GetFiles(@"D:/Names/AllFiles").Where(p => p.Contains(value.ToString()) && p.Contains("FirstNames"));
+            var firstNames = new List<string>();
+
+            foreach (var file in filesPathes1)
+            {
+                List<string> firstNamesPart = JsonConvert.DeserializeObject<List<string>>(File.ReadAllText(file));
+                firstNames.AddRange(firstNamesPart);
+            }
+
+            if (firstNames.Count > 0)
+            {
+                firstNames = new List<string>(firstNames.OrderBy(s => s));
+                var modifiedFirstNames = new List<string>();
+                foreach (var name in firstNames)
+                {
+                    var n = Regex.Replace(name, pattern, String.Empty);
+
+                    if (!n.Contains("#") && !n.Contains("&")) modifiedFirstNames.Add(n);
+                }
+
+                shitCount += modifiedFirstNames.Count(s => s.Contains('('));
+                using (var file = File.CreateText(@"D:/Names/AllFiles/JoinFiles/" + value + ".FirstNames.Join.json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, modifiedFirstNames);
+                }
+            }
+
+            //------------
+            var filesPathes2 = Directory.GetFiles(@"D:/Names/AllFiles").Where(p => p.Contains(value.ToString()) && p.Contains("LastNames"));
+            var lastNames = new List<string>();
+
+            foreach (var file in filesPathes2)
+            {
+                List<string> lastNamesPart = JsonConvert.DeserializeObject<List<string>>(File.ReadAllText(file));
+                lastNames.AddRange(lastNamesPart);
+            }
+
+            if (lastNames.Count > 0)
+            {
+                lastNames = new List<string>(lastNames.OrderBy(s => s));
+                var modifiedLastNames = new List<string>();
+                foreach (var name in lastNames)
+                {
+                    var n = Regex.Replace(name, pattern, String.Empty);
+
+                    if (!n.Contains("#") && !n.Contains("&")) modifiedLastNames.Add(n);
+                }
+                shitCount += modifiedLastNames.Count(s => s.Contains('('));
+                using (var file = File.CreateText(@"D:/Names/AllFiles/JoinFiles/" + value + ".LastNames.Join.json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, modifiedLastNames);
+                }
+            }
+
+            var i = shitCount;
+        }
+    }
+}
