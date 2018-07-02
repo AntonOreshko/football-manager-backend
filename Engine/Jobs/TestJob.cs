bd8a4df20806942d389aeeba7b2ce3377fccf989
araxis <oreshko2010@gmail.com>
2018-07-02 05:33:00 +0300

    йгфкея

---
+++ Engine/Jobs/TestJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace Engine.Jobs
+{
+    public class TestJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync("Hello world");
+        }
+    }
+}
--- Engine/Program.cs
+++ Engine/Program.cs
@@ -1,8 +1,14 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Threading.Tasks;
+using Engine.Jobs;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using RepositoryLayer.EntityFramework.Context;
 using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using Quartz.Impl;
 
 namespace Engine
 {
@@ -27,6 +33,46 @@ namespace Engine
             services.AddDbContext<FootballManagerContext>(o => o.UseSqlServer(config.GetConnectionString(connectionName)));
 
             var serviceProvider = services.BuildServiceProvider();
+
+            var props = new NameValueCollection
+            {
+                { "quartz.serializer.type", "binary" }
+            };
+            var factory = new StdSchedulerFactory(props);
+
+            StartQuartz();
+
+            Console.ReadLine();
+        }
+
+        private static async Task StartQuartz()
+        {
+            // construct a scheduler factory
+            var props = new NameValueCollection
+            {
+                { "quartz.serializer.type", "binary" }
+            };
+            var factory = new StdSchedulerFactory(props);
+
+            // get a scheduler
+            var sched = await factory.GetScheduler();
+            await sched.Start();
+
+            // define the job and tie it to our HelloJob class
+            var job = JobBuilder.Create<TestJob>()
+                .WithIdentity("myJob", "group1")
+                .Build();
+
+            // Trigger the job to run now, and then every 2 seconds
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("myTrigger", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(2)
+                    .RepeatForever())
+                .Build();
+
+            await sched.ScheduleJob(job, trigger);
         }
     }
 }
